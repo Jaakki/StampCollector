3cfc79dad8284803106d289a4cd4ae8e4d5fb974
Jaakki <62286646+Jaakki@users.noreply.github.com>
2020-05-28 09:20:05 +0300

    Updates

---
+++ precache-manifest.f907efb39d5c7a56be55c0265f2d11f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b401df53fd00f4f87362f8191544e841",
+    "url": "/StampCollector/index.html"
+  },
+  {
+    "revision": "026d0e6643d71f60044b",
+    "url": "/StampCollector/static/css/main.94a5ff8a.chunk.css"
+  },
+  {
+    "revision": "3c31f4b2342669f3970f",
+    "url": "/StampCollector/static/js/2.80311724.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StampCollector/static/js/2.80311724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026d0e6643d71f60044b",
+    "url": "/StampCollector/static/js/main.0259c750.chunk.js"
+  },
+  {
+    "revision": "b8af07da35dc28d6ded2",
+    "url": "/StampCollector/static/js/runtime-main.06d06c70.js"
+  }
+]);
